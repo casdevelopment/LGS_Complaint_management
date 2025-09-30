0fe6ff9c41e361ab064bc76d69fe8ff05cd861be
muhammadali099 <muhammadaliamir177@gmail.com>
2025-09-30 18:00:07 +0300

    Fix crash issue

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import messaging from '@react-native-firebase/messaging';
-import PushNotification from 'react-native-push-notification';
+import notifee from '@notifee/react-native';
 
 // Background handler
 messaging().setBackgroundMessageHandler(async remoteMessage => {
@@ -16,19 +16,30 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
 // Foreground handler
 messaging().onMessage(async remoteMessage => {
   console.log('Foreground message:', remoteMessage);
-
   const { data, notification } = remoteMessage;
   const title = data?.title || notification?.title || 'Notification';
   const body = data?.body || notification?.body || 'You have a new message';
-
-  PushNotification.localNotification({
-    channelId: '2',
-    title,
-    message: body,
-    userInfo: data,
-    playSound: true,
-    soundName: 'default',
-  });
+  onDisplayNotification(title, body);
 });
+const onDisplayNotification = async (title, body) => {
+  // Create a channel (required for Android)
+  const channelId = await notifee.createChannel({
+    id: 'default',
+    name: 'Default Channel',
+  });
+
+  // Display a notification
+  await notifee.displayNotification({
+    title: title,
+    body: body,
+    android: {
+      channelId,
+      // smallIcon: 'ic_launcher',
+      pressAction: {
+        id: 'default',
+      },
+    },
+  });
+};
 
 AppRegistry.registerComponent(appName, () => App);
